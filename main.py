a1d998550d2f6532f678f4e2c3457ee9adc6c87c
Julian <julian_merkofer@gmx.ch>
2018-05-03 15:03:56 +0200

    model...

--- main.py
+++ main.py
@@ -176,78 +176,17 @@ for i in range(dataSet.shape[0]):
 #   converting the input class labels to categorical labels for training   #
 #**************************************************************************#
 #trainY = to_categorical(trainX[:, dict['popularityScore']], num_classes = 2)
+#testY = to_categorical(testX[:, dict['popularityScore']], num_classes = 2)
 
 # ignoring categorization
 trainY = trainX[:, dict['popularityScore']]
+testY = testX[:, dict['popularityScore']]
 
 
 #*******************#
 #   defines model   #
 #*******************#
 x = Input((imgTrainX.shape[1], imgTrainX.shape[2], imgDataSet.shape[3]))
-
-
-# "successful" approach #
-#***********************#
-# ??? overfitting... (what can we do? bigger data set?)
-
-# ~ 66% accuracy (32x32, batch=128, epochs=10, t > 75s)
-# ~ 74% accuracy, ~ 69% val_acc (32x32, batch=16, epochs=20, t > 300s)
-# ~ 99% accuracy, ~ 64% val_acc (32x32, batch=16, epochs=60, t > 800s)
-
-# ~ 67% accuracy (64x64, batch=128, epochs=10, t > 300s)
-# ~ 72% accuracy, ~ 69% val_acc (128x128, batch=16, epochs=10, t > 300s)
-# ~ 88% accuracy, ~ 65% val_acc (64x64, batch=16, epochs=20, t > 750s)
-
-# ~ 80% accuracy, ~ 68% val_acc (128x128, batch=16, epochs=10, t > 2000s)
-# ~ 99% accuracy, ~ 65% val_acc (128x128, batch=16, epochs=20, t > 3600s)
-
-#y = Conv2D(filters=16, kernel_size=(3, 3), activation='relu')(x)
-#y = MaxPool2D(pool_size=(3, 3))(y)
-
-#y = Flatten()(y)
-
-#y = Dense(128, activation='relu')(y)
-#y = Dense(2, activation='softmax')(y)
-
-
-# "successful" approach, trying not to overfit #
-#**********************************************#
-# ??? converges to values below...
-
-# ~ 70% accuracy, ~ 69% val_acc (32x32, batch=16, epochs=20, t > 200s)
-# ~ 71% accuracy, ~ 68% val_acc (64x64, batch=16, epochs=20, t > 600s)
-# ~ 76% accuracy, ~ 65% val_acc (128x128, batch=16, epochs=20, t > 3000s)
-
-## ~ 72% accuracy, ~ 68% val_acc (32x32, batch=16, epochs=20, t > 1000s)
-
-##y = Conv2D(filters=32, kernel_size=(3, 3), activation='relu')(x)
-##y = MaxPool2D(pool_size=(3, 3))(y)
-#y = Conv2D(filters=16, kernel_size=(3, 3), activation='relu')(y)
-#y = MaxPool2D(pool_size=(3, 3))(y)
-
-#y = Flatten()(y)
-
-##y = Dense(64, activation='relu')(y)
-#y = Dense(2, activation='softmax')(y)
-
-
-# new approach (loading image data set as float32 and dividing it by 255) #
-#*************************************************************************#
-# ~ 68% accuracy, ~ 68% val_acc (32x32 COLOR, batch=128, epochs=10, t > 750s)
-# ~ 68% accuracy, ~ 67% val_acc (64x64 COLOR, batch=128, epochs=10, t > 8000s)
-
-# ~ 86% accuracy, ~ 64% val_acc (32x32 COLOR, batch=16, epochs=20, t > 1500s)
-# -> after 16 epochs (acc at 80% and val_acc 69%) the val_acc starts to drop
-
-#y = Conv2D(filters=128, kernel_size=(3, 3), activation='relu')(x)
-#y = MaxPool2D(pool_size=(3, 3))(y)
-
-#y = Flatten()(y)
-
-#y = Dense(128, activation='relu')(y)
-#y = Dense(128, activation='relu')(y)
-#y = Dense(2, activation='softmax')(y)
 
 
 # desperate approaches #
@@ -268,31 +207,18 @@ x = Input((imgTrainX.shape[1], imgTrainX.shape[2], imgDataSet.shape[3]))
 #y = Dense(2, activation='softmax')(y)
 
 
-# testing dropout approach #
-#**************************#
-
-#y = Conv2D(filters=16, kernel_size=(3, 3), activation='relu')(x)
-#y = MaxPool2D(pool_size=(3, 3))(y)
-
-#y = Flatten()(y)
-
-#y = Dense(64, activation='relu')(y)
-#y = Dropout(rate=0.2)(y)
-#y = Dense(1, activation='sigmoid')(y)
-
-
 # testing denseNet #
 #******************#
 
-y = Conv2D(filters=8, kernel_size=(7, 7), activation='relu')(x)
+y = Conv2D(filters=16, kernel_size=(7, 7), activation='relu')(x)
+y = Dense(320, activation='relu')(y)
+y = Conv2D(filters=32, kernel_size=(5, 5), activation='relu')(y)
+y = MaxPool2D(pool_size=(3, 3))(y)
 y = Dense(160, activation='relu')(y)
-y = Conv2D(filters=16, kernel_size=(5, 5), activation='relu')(y)
-y = MaxPool2D(pool_size=(2, 2))(y)
-y = Dense(80, activation='relu')(y)#
-y = Conv2D(filters=32, kernel_size=(3, 3), activation='relu')(y)
-y = MaxPool2D(pool_size=(2, 2))(y)
-y = Dense(40, activation='relu')(y)
-y = MaxPool2D(pool_size=(2, 2))(y)
+y = Conv2D(filters=64, kernel_size=(3, 3), activation='relu')(y)
+y = MaxPool2D(pool_size=(3, 3))(y)
+y = Dense(80, activation='relu')(y)
+y = MaxPool2D(pool_size=(3, 3))(y)
 y = Flatten()(y)
 y = Dense(1, activation='sigmoid')(y)
 
@@ -310,7 +236,8 @@ checkpoint = ModelCheckpoint(filepath='model.h5', save_best_only=True)
 
 model.compile(optimizer=sgd, loss='binary_crossentropy', metrics=['accuracy'])
 
-model.fit(x=imgTrainX, y=trainY, batch_size=128, epochs=60, validation_split=0.2,
+batchSize = 128
+model.fit(x=imgTrainX, y=trainY, batch_size=batchSize, epochs=60, validation_split=0.2,
           callbacks=[checkpoint])
 print "time spent on training:", time.time() - tick, "s"
 tick = time.time()
@@ -319,14 +246,7 @@ tick = time.time()
 #*****************#
 #   tests model   #
 #*****************#
-correct = 0
-testXvis = imgTestX
-for i in range(testXvis.shape[0]):
-    # ??? why is this so slow?
-    correct += (model.predict(testXvis, batch_size=128) > 0.5)[:, 0].astype(np.int)[i]
-
-print "accuracy of model with test set:", float(correct) / testXvis.shape[0],\
-    "(", time.time() - tick, "s )"
+model.evaluate(imgTestX, testY, batch_size=batchSize)
 
 
 #*****************************************#
@@ -339,7 +259,7 @@ preds = (model.predict(testXvis) > 0.5)[:, 0].astype(np.int)
 
 labels = ["bad", "good"]
 
-# removes dimension for plotting
+# removes dimension for plotting (use when color channel 1)
 #imgTestX = np.squeeze(imgTestX, axis=3)
 
 plt.figure()
@@ -350,3 +270,4 @@ for i in xrange(3):
         plt.title("Ground Truth: %s, \n Prediction %s" %
                   (labels[groundTruths[5*i + j]], labels[preds[5*i + j]]))
 plt.show()
+
